1a18300478fc7c37b5205d1302cb97ca6ccc268e
Lukas Alexandersson <alexandersson98l@gmail.com>
2026-03-19 16:06:29 +0100

    implement global exception handling with ApiErrorResponse and GlobalExceptionHandler

---
+++ src/main/java/com/example/boilerroom_labb1/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.boilerroom_labb1.exceptions;
+
+
+import com.example.boilerroom_labb1.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ErrorResponse>handleBookNotFound(
+            BookNotFoundException ex,
+            HttpServletRequest request
+    ){
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
