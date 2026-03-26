3a813424b5f8cca5d68f42be8e80fec0556eb44d
Lukas Alexandersson <alexandersson98l@gmail.com>
2026-03-26 12:18:40 +0100

    feat: implement POST /authors with request, response, mapper and controller

--- src/main/java/com/example/boilerroom_labb1/controller/AuthorController.java
+++ src/main/java/com/example/boilerroom_labb1/controller/AuthorController.java
@@ -1,4 +1,30 @@
 package com.example.boilerroom_labb1.controller;
 
+
+import com.example.boilerroom_labb1.dto.AuthorRequestDto;
+import com.example.boilerroom_labb1.dto.AuthorResponseDto;
+import com.example.boilerroom_labb1.service.AuthorService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/author")
 public class AuthorController {
+    private final AuthorService service;
+
+    public AuthorController (AuthorService service){
+        this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<AuthorResponseDto>create(@Valid @RequestBody AuthorRequestDto request){
+        AuthorResponseDto response = service.createEntity(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
 }
--- src/main/java/com/example/boilerroom_labb1/mapper/AuthorMapper.java
+++ src/main/java/com/example/boilerroom_labb1/mapper/AuthorMapper.java
@@ -1,4 +1,38 @@
 package com.example.boilerroom_labb1.mapper;
 
+import com.example.boilerroom_labb1.dto.AuthorRequestDto;
+import com.example.boilerroom_labb1.dto.AuthorResponseDto;
+import com.example.boilerroom_labb1.dto.BookResponseDto;
+import com.example.boilerroom_labb1.entity.Author;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class AuthorMapper {
+
+
+    private final BookMapper bookMapper;
+
+    public AuthorMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public AuthorResponseDto toResponseDto(Author author){
+        List<BookResponseDto> books = author.getBooks().stream()
+                .map(bookMapper::toResponseDto)
+                .toList();
+
+        return new AuthorResponseDto(
+                author.getId(),
+                author.getName(),
+                books
+        );
+    }
+
+
+
+    public Author toEntity(AuthorRequestDto request){
+        return new Author(request.name());
+    }
 }
