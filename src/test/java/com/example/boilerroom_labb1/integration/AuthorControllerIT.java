7e0b1094475ac198cbc48620c4f04c6f46e15dd3
Lukas Alexandersson <alexandersson98l@gmail.com>
2026-03-30 19:33:38 +0200

    test: add integrationtest for getAuthorById

--- src/test/java/com/example/boilerroom_labb1/integration/AuthorControllerIT.java
+++ src/test/java/com/example/boilerroom_labb1/integration/AuthorControllerIT.java
@@ -47,4 +47,25 @@ public class AuthorControllerIT {
         assertEquals("Herbert Bengtsson", body.name());
     }
 
+    @Test
+    void shouldReturnAuthorWithId(){
+        AuthorRequestDto authorRequest = new AuthorRequestDto("Herbert Bengtsson");
+        ResponseEntity<AuthorResponseDto>authorResponse = restTemplate.postForEntity("/api/v1/author",
+                authorRequest,
+                AuthorResponseDto.class);
+
+        Long authorId = authorResponse.getBody().id();
+        ResponseEntity<AuthorResponseDto>getById = restTemplate.getForEntity("/api/v1/author/{id}",
+                AuthorResponseDto.class,
+                authorId);
+
+        assertEquals(HttpStatus.OK, getById.getStatusCode());
+        AuthorResponseDto fetchedBody = getById.getBody();
+        assertNotNull(fetchedBody);
+        assertEquals("Herbert Bengtsson",fetchedBody.name());
+        assertEquals(authorResponse.getBody().id(), fetchedBody.id());
+
+
+    }
+
 }
