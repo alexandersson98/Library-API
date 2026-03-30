1d55688deac9c634d95949e9d35635ba27f2dcac
Lukas Alexandersson <alexandersson98l@gmail.com>
2026-03-30 18:07:01 +0200

    test(loan): add integration test for creating and fetching loan

--- src/test/java/com/example/boilerroom_labb1/integration/AuthorBookIT.java
+++ src/test/java/com/example/boilerroom_labb1/integration/AuthorBookIT.java
@@ -5,6 +5,8 @@ import com.example.boilerroom_labb1.dto.author.AuthorRequestDto;
 import com.example.boilerroom_labb1.dto.author.AuthorResponseDto;
 import com.example.boilerroom_labb1.dto.book.BookRequestDto;
 import com.example.boilerroom_labb1.dto.book.BookResponseDto;
+import com.example.boilerroom_labb1.dto.loan.LoanRequestDto;
+import com.example.boilerroom_labb1.dto.loan.LoanResponseDto;
 import com.example.boilerroom_labb1.repository.AuthorRepository;
 import com.example.boilerroom_labb1.repository.BookRepository;
 import com.example.boilerroom_labb1.repository.LoanRepository;
@@ -17,8 +19,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -66,6 +70,54 @@ public class AuthorBookIT {
         assertEquals("edee-333", bookResponse.getBody().isbn());
         assertEquals("Gunnar Larsson", bookResponse.getBody().authorName());
         assertEquals(2005, bookResponse.getBody().publishedYear());
+    }
+
+
+    @Test
+    void shouldCreateAuthorBookAndLoan_AndReturnCreatedLoan(){
+        AuthorRequestDto authorRequest = new AuthorRequestDto("Johan Bengtsson");
+
+        ResponseEntity<AuthorResponseDto>authorResponse = restTemplate.postForEntity("/api/v1/author",
+                authorRequest,
+                AuthorResponseDto.class);
+
+         Long authorId = authorResponse.getBody().id();
+
+         BookRequestDto bookRequest = new BookRequestDto("The hunger games", authorId, "eeee", 2005);
+          ResponseEntity<BookResponseDto> bookResponse = restTemplate.postForEntity("/api/v1/books",
+                 bookRequest,
+                 BookResponseDto.class);
+
+          Long bookId = bookResponse.getBody().id();
+
+        LoanRequestDto loanRequest = new LoanRequestDto(bookId);
+        ResponseEntity<LoanResponseDto> loanResponse = restTemplate.postForEntity("/api/v1/loans",
+                loanRequest,
+                LoanResponseDto.class);
+
+        assertEquals(HttpStatus.CREATED, loanResponse.getStatusCode());
+        assertNotNull(loanResponse.getBody());
+        assertNotNull(loanResponse.getBody().id());
+
+        Long createdLoanId = loanResponse.getBody().id();
+
+
+        ResponseEntity<LoanResponseDto[]>loanResponseDto = restTemplate.getForEntity("/api/v1/loans",
+                LoanResponseDto[].class);
+
+
+        assertEquals(HttpStatus.OK, loanResponseDto.getStatusCode());
+        assertNotNull(loanResponseDto);
+
+        LoanResponseDto[] loans = loanResponseDto.getBody();
+
+        Boolean found = Arrays.stream(loans)
+                .anyMatch(loan -> loan.id().equals(createdLoanId));
+
+
+        assertTrue(found);
+
+
     }
 
 
